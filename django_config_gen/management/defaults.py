83bac63a893d1caf555e3d00b6447c4fd8600f34
Brillgen Administrator <admin@brillgen.com>
2017-06-10 09:50:39 +0530

    Making django.contrib.sites optional as it should not be necessary for this reusable app

--- django_config_gen/management/defaults.py
+++ django_config_gen/management/defaults.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-#Copyright (C) 2010, 2011 Seán Hayes
+# Copyright (C) 2010, 2011 Seán Hayes
 #
-#Licensed under a BSD 3-Clause License. See LICENSE file.
+# Licensed under a BSD 3-Clause License. See LICENSE file.
+
+import os
+
+from django.db import models, transaction
 
 import __main__
-import os
-from django.contrib.sites.models import Site
-from django.db import transaction
 
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__main__.__file__))
@@ -19,13 +20,8 @@ GENERATED_DIR = os.path.join(_config_dir, 'generated')
 CONTEXT_PROCESSORS = []
 
 
-# Error is thrown when running "./manage.py syncdb" for the first time and this app is installed.
 try:
+    from django.contrib.sites.models import Site
     HOST = Site.objects.get_current().domain.split(':')[0]
-except:
-    # try/except to avoid "django.db.transaction.TransactionManagementError: This code isn't under transaction management"
-    try:
-        transaction.rollback()
-    except:
-        pass
+except (RuntimeError, models.ObjectDoesNotExist):
     HOST = 'localhost'
